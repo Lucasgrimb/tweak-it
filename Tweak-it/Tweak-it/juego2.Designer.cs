cfc4a937074eeeeaf990eb7616b33d79cad3042a
Lucasgrimb <108102466+Lucasgrimb@users.noreply.github.com>
2022-10-14 15:55:52 -0300

    creo juego 2

--- Tweak-it/Tweak-it/juego2.Designer.cs
+++ Tweak-it/Tweak-it/juego2.Designer.cs
@@ -28,12 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pB1p = new System.Windows.Forms.PictureBox();
+            this.pB2p = new System.Windows.Forms.PictureBox();
+            this.pB3p = new System.Windows.Forms.PictureBox();
+            this.pB4p = new System.Windows.Forms.PictureBox();
+            this.pB5p = new System.Windows.Forms.PictureBox();
+            this.pB5d = new System.Windows.Forms.PictureBox();
+            this.pB4d = new System.Windows.Forms.PictureBox();
+            this.pB3d = new System.Windows.Forms.PictureBox();
+            this.pB2d = new System.Windows.Forms.PictureBox();
+            this.pB1d = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB3p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB4p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB5p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB5d)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB4d)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB3d)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2d)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1d)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pB1p
+            // 
+            this.pB1p.Location = new System.Drawing.Point(127, 12);
+            this.pB1p.Name = "pB1p";
+            this.pB1p.Size = new System.Drawing.Size(101, 91);
+            this.pB1p.TabIndex = 0;
+            this.pB1p.TabStop = false;
+            this.pB1p.Click += new System.EventHandler(this.pB1p_Click);
+            // 
+            // pB2p
+            // 
+            this.pB2p.Location = new System.Drawing.Point(127, 121);
+            this.pB2p.Name = "pB2p";
+            this.pB2p.Size = new System.Drawing.Size(101, 91);
+            this.pB2p.TabIndex = 1;
+            this.pB2p.TabStop = false;
+            this.pB2p.Click += new System.EventHandler(this.pB2p_Click);
+            // 
+            // pB3p
+            // 
+            this.pB3p.Location = new System.Drawing.Point(127, 239);
+            this.pB3p.Name = "pB3p";
+            this.pB3p.Size = new System.Drawing.Size(101, 91);
+            this.pB3p.TabIndex = 2;
+            this.pB3p.TabStop = false;
+            this.pB3p.Click += new System.EventHandler(this.pB3p_Click);
+            // 
+            // pB4p
+            // 
+            this.pB4p.Location = new System.Drawing.Point(127, 347);
+            this.pB4p.Name = "pB4p";
+            this.pB4p.Size = new System.Drawing.Size(101, 91);
+            this.pB4p.TabIndex = 3;
+            this.pB4p.TabStop = false;
+            this.pB4p.Click += new System.EventHandler(this.pB4p_Click);
+            // 
+            // pB5p
+            // 
+            this.pB5p.Location = new System.Drawing.Point(127, 474);
+            this.pB5p.Name = "pB5p";
+            this.pB5p.Size = new System.Drawing.Size(101, 91);
+            this.pB5p.TabIndex = 4;
+            this.pB5p.TabStop = false;
+            this.pB5p.Click += new System.EventHandler(this.pB5p_Click);
+            // 
+            // pB5d
+            // 
+            this.pB5d.Location = new System.Drawing.Point(521, 474);
+            this.pB5d.Name = "pB5d";
+            this.pB5d.Size = new System.Drawing.Size(101, 91);
+            this.pB5d.TabIndex = 9;
+            this.pB5d.TabStop = false;
+            this.pB5d.Click += new System.EventHandler(this.pB5d_Click);
+            // 
+            // pB4d
+            // 
+            this.pB4d.Location = new System.Drawing.Point(521, 347);
+            this.pB4d.Name = "pB4d";
+            this.pB4d.Size = new System.Drawing.Size(101, 91);
+            this.pB4d.TabIndex = 8;
+            this.pB4d.TabStop = false;
+            this.pB4d.Click += new System.EventHandler(this.pB4d_Click);
+            // 
+            // pB3d
+            // 
+            this.pB3d.Location = new System.Drawing.Point(521, 239);
+            this.pB3d.Name = "pB3d";
+            this.pB3d.Size = new System.Drawing.Size(101, 91);
+            this.pB3d.TabIndex = 7;
+            this.pB3d.TabStop = false;
+            this.pB3d.Click += new System.EventHandler(this.pB3d_Click);
+            // 
+            // pB2d
+            // 
+            this.pB2d.Location = new System.Drawing.Point(521, 121);
+            this.pB2d.Name = "pB2d";
+            this.pB2d.Size = new System.Drawing.Size(101, 91);
+            this.pB2d.TabIndex = 6;
+            this.pB2d.TabStop = false;
+            this.pB2d.Click += new System.EventHandler(this.pB2d_Click);
+            // 
+            // pB1d
+            // 
+            this.pB1d.Location = new System.Drawing.Point(521, 12);
+            this.pB1d.Name = "pB1d";
+            this.pB1d.Size = new System.Drawing.Size(101, 91);
+            this.pB1d.TabIndex = 5;
+            this.pB1d.TabStop = false;
+            this.pB1d.Click += new System.EventHandler(this.pB1d_Click);
+            // 
+            // juego2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 597);
+            this.Controls.Add(this.pB5d);
+            this.Controls.Add(this.pB4d);
+            this.Controls.Add(this.pB3d);
+            this.Controls.Add(this.pB2d);
+            this.Controls.Add(this.pB1d);
+            this.Controls.Add(this.pB5p);
+            this.Controls.Add(this.pB4p);
+            this.Controls.Add(this.pB3p);
+            this.Controls.Add(this.pB2p);
+            this.Controls.Add(this.pB1p);
+            this.Name = "juego2";
             this.Text = "juego2";
+            this.Load += new System.EventHandler(this.juego2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pB1p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB3p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB4p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB5p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB5d)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB4d)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB3d)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2d)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1d)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pB1p;
+        private System.Windows.Forms.PictureBox pB2p;
+        private System.Windows.Forms.PictureBox pB3p;
+        private System.Windows.Forms.PictureBox pB4p;
+        private System.Windows.Forms.PictureBox pB5p;
+        private System.Windows.Forms.PictureBox pB5d;
+        private System.Windows.Forms.PictureBox pB4d;
+        private System.Windows.Forms.PictureBox pB3d;
+        private System.Windows.Forms.PictureBox pB2d;
+        private System.Windows.Forms.PictureBox pB1d;
     }
 }
--- Tweak-it/Tweak-it/juego2.cs
+++ Tweak-it/Tweak-it/juego2.cs
@@ -12,9 +12,209 @@ namespace Tweak_it
 {
     public partial class juego2 : Form
     {
+        PictureBox[] pBpictos = new PictureBox[5];
+        PictureBox[] pBdibujos = new PictureBox[5];
+        List<Image> pictos = new List<Image>();
+        List<Image> dibujos = new List<Image>();
+        List<Image> ndibujos = new List<Image>();
+        Random rand = new Random();
+        String aux;
+        int cont = 0;
         public juego2()
         {
             InitializeComponent();
+        }
+
+        private void juego2_Load(object sender, EventArgs e)
+        {
+            //agrego Pb al arrray
+            pBpictos[0] = pB1p;
+            pBpictos[1] = pB2p;
+            pBpictos[2] = pB3p;
+            pBpictos[3] = pB4p;
+            pBpictos[4] = pB5p;
+
+
+            pBdibujos[0] = pB1d;
+            pBdibujos[1] = pB2d;
+            pBdibujos[2] = pB3d;
+            pBdibujos[3] = pB4d;
+            pBdibujos[4] = pB5d;
+
+            // pongo las imagenes en un formato especial para que se vean centradas
+            for (int x = 0; x < pBpictos.Length; x++)
+            {
+                pBpictos[x].SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            for (int x = 0; x < pBdibujos.Length; x++)
+            {
+                pBdibujos[x].SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+
+            randomizarImagenes();
+        }
+        private void randomizarImagenes()
+        {
+            // Agrego las imagenes de resources a la lista con las emociones que se usarán en el nivel
+            pictos.Add(Tweak_it.Properties.Resources.contento);
+            pictos.Add(Tweak_it.Properties.Resources.triste);
+            pictos.Add(Tweak_it.Properties.Resources.enfadado);
+            pictos.Add(Tweak_it.Properties.Resources.asustado);
+            pictos.Add(Tweak_it.Properties.Resources.sorprendido);
+            pictos.Add(Tweak_it.Properties.Resources.cansado);
+            pictos.Add(Tweak_it.Properties.Resources.hambriento);
+            pictos.Add(Tweak_it.Properties.Resources.sediento);
+            pictos.Add(Tweak_it.Properties.Resources.enfermo);
+            pictos.Add(Tweak_it.Properties.Resources.aburrido);
+            pictos.Add(Tweak_it.Properties.Resources.preocupado);
+
+            dibujos.Add(Tweak_it.Properties.Resources.contentoD);
+            dibujos.Add(Tweak_it.Properties.Resources.tristeD);
+            dibujos.Add(Tweak_it.Properties.Resources.enfadadoD);
+            dibujos.Add(Tweak_it.Properties.Resources.asustadoD);
+            dibujos.Add(Tweak_it.Properties.Resources.sorprendidoD);
+            dibujos.Add(Tweak_it.Properties.Resources.cansadoD);
+            dibujos.Add(Tweak_it.Properties.Resources.hambrientoD);
+            dibujos.Add(Tweak_it.Properties.Resources.sedientoD);
+            dibujos.Add(Tweak_it.Properties.Resources.enfermoD);
+            dibujos.Add(Tweak_it.Properties.Resources.aburridoD);
+            dibujos.Add(Tweak_it.Properties.Resources.preocupadoD);
+
+            pictos[0].Tag = "contento";
+            pictos[1].Tag = "triste";
+            pictos[2].Tag = "enfadado";
+            pictos[3].Tag = "asustado";
+            pictos[4].Tag = "sorprendido";
+            pictos[5].Tag = "cansado";
+            pictos[6].Tag = "hambriento";
+            pictos[7].Tag = "sediento";
+            pictos[8].Tag = "enfermo";
+            pictos[9].Tag = "aburrido";
+            pictos[10].Tag = "preocupado";
+
+
+            dibujos[0].Tag = "contento";
+            dibujos[1].Tag = "triste";
+            dibujos[2].Tag = "enfadado";
+            dibujos[3].Tag = "asustado";
+            dibujos[4].Tag = "sorprendido";
+            dibujos[5].Tag = "cansado";
+            dibujos[6].Tag = "hambriento";
+            dibujos[7].Tag = "sediento";
+            dibujos[8].Tag = "enfermo";
+            dibujos[9].Tag = "aburrido";
+            dibujos[10].Tag = "preocupado";
+
+
+            //asigno emociones a pBpictos
+
+            for (int i = 0; i < 5; i++)
+            {
+                int x = rand.Next(0, pictos.Count);
+
+                pBpictos[i].Image = pictos[x];
+                pBpictos[i].Tag = i.ToString();
+                ndibujos.Add(dibujos[x]);
+                pictos.RemoveAt(x);
+                dibujos.RemoveAt(x);
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                int f = rand.Next(0, ndibujos.Count);
+                pBdibujos[j].Image = ndibujos[f];
+                ndibujos.RemoveAt(f);
+            }
+
+        }
+
+        private void pB1p_Click(object sender, EventArgs e)
+        {
+            aux = pB1p.Image.Tag.ToString();
+        }
+
+        private void pB2p_Click(object sender, EventArgs e)
+        {
+            aux = pB2p.Image.Tag.ToString();
+        }
+
+        private void pB3p_Click(object sender, EventArgs e)
+        {
+            aux = pB3p.Image.Tag.ToString();
+        }
+
+        private void pB4p_Click(object sender, EventArgs e)
+        {
+            aux = pB4p.Image.Tag.ToString();
+        }
+
+        private void pB5p_Click(object sender, EventArgs e)
+        {
+            aux = pB5p.Image.Tag.ToString();
+        }
+
+        private void pB1d_Click(object sender, EventArgs e)
+        {
+            if (pB1d.Image.Tag.ToString() == aux)
+            {
+                MessageBox.Show("Correcto");
+                cont++;
+            }
+            else
+            {
+                MessageBox.Show("incorrecto, volve a intentarlo");
+            }
+        }
+
+        private void pB2d_Click(object sender, EventArgs e)
+        {
+            if (pB2d.Image.Tag.ToString() == aux)
+            {
+                MessageBox.Show("Correcto");
+                cont++;
+            }
+            else
+            {
+                MessageBox.Show("incorrecto, volve a intentarlo");
+            }
+        }
+
+        private void pB3d_Click(object sender, EventArgs e)
+        {
+            if (pB3d.Image.Tag.ToString() == aux)
+            {
+                MessageBox.Show("Correcto");
+                cont++;
+            }
+            else
+            {
+                MessageBox.Show("incorrecto, volve a intentarlo");
+            }
+        }
+
+        private void pB4d_Click(object sender, EventArgs e)
+        {
+            if (pB4d.Image.Tag.ToString() == aux)
+            {
+                MessageBox.Show("Correcto");
+                cont++;
+            }
+            else
+            {
+                MessageBox.Show("incorrecto, volve a intentarlo");
+            }
+        }
+
+        private void pB5d_Click(object sender, EventArgs e)
+        {
+            if (pB5d.Image.Tag.ToString() == aux)
+            {
+                MessageBox.Show("Correcto");
+                cont++;
+            }
+            else
+            {
+                MessageBox.Show("incorrecto, volve a intentarlo");
+            }
         }
     }
 }
