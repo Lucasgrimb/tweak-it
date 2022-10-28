3403f9ac1403dcfc1fca5cc36a7bce143596436c
BrunoAdaniya <48045008@est.ort.edu.ar>
2022-10-28 17:32:55 -0300

    hacer el diseño

---
+++ Tweak-it/Tweak-it/LOGIN.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tweak_it
+{
+    public partial class LOGIN : Form
+    {
+        public LOGIN()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNombre_Enter(object sender, EventArgs e)
+        {
+            if(txtNombre.Text == "NOMBRE ")
+            {
+                txtNombre.Text = "";
+                txtNombre.ForeColor = Color.LightGray ;
+            }
+        }
+
+        private void txtNombre_Leave(object sender, EventArgs e)
+        {
+            if(txtNombre.Text == "")
+            {
+                txtNombre.Text = "NOMBRE";
+                txtNombre.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void txtApellido_Enter(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "APELLIDO")
+            {
+                txtNombre.Text = "";
+                txtNombre.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtApellido_Leave(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "")
+            {
+                txtNombre.Text = "APELLIDO";
+                txtNombre.ForeColor = Color.DimGray;
+            }
+        }
+    }
+}
---
+++ Tweak-it/Tweak-it/LOGIN_PROFESOR.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Tweak_it
+{
+    partial class LOGIN_PROFESOR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.Color.Silver;
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.txtUser.ForeColor = System.Drawing.Color.DimGray;
+            this.txtUser.HideSelection = false;
+            this.txtUser.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtUser.Location = new System.Drawing.Point(737, 458);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(445, 94);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "\r\nUSUARIO";
+            this.txtUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtUser.TextChanged += new System.EventHandler(this.txtUser_TextChanged);
+            this.txtUser.Enter += new System.EventHandler(this.txtUser_Enter);
+            this.txtUser.Leave += new System.EventHandler(this.txtUser_Leave);
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.Color.Silver;
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.txtPass.ForeColor = System.Drawing.Color.DimGray;
+            this.txtPass.HideSelection = false;
+            this.txtPass.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtPass.Location = new System.Drawing.Point(737, 623);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(445, 94);
+            this.txtPass.TabIndex = 2;
+            this.txtPass.Text = "\r\nCONTRASEÑA";
+            this.txtPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPass.Enter += new System.EventHandler(this.txtPass_Enter);
+            this.txtPass.Leave += new System.EventHandler(this.txtPass_Leave);
+            // 
+            // LOGIN_PROFESOR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::Tweak_it.Properties.Resources.fondo___texto1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LOGIN_PROFESOR";
+            this.Text = "Crear_Usuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+    }
+}
