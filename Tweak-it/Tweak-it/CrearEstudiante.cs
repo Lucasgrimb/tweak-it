9994a614314d399c57b491b6f5d161341a1c82c5
BrunoAdaniya <48045008@est.ort.edu.ar>
2022-10-31 18:53:44 -0300

    hice el crear estudiante

--- Tweak-it/Tweak-it/CrearEstudiante.cs
+++ Tweak-it/Tweak-it/CrearEstudiante.cs
@@ -13,28 +13,47 @@ namespace Tweak_it
 {
     public partial class CrearEstudiante : Form
     {
-        OleDbConnection connection;
+        OleDbConnection connection = new OleDbConnection();
         public CrearEstudiante()
         {
             InitializeComponent();
         }
 
-        private void CrearEstudiantes_Load(object sender, EventArgs e)
+        private void txtNombre_Enter(object sender, EventArgs e)
         {
-            connection = new OleDbConnection();
-            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\user\Documents\GitHub\tweak-it\Tweak-it\BDD Tweak-It.accdb;Persist Security Info=False";
+            if(txtNombre.Text == "NOMBRE")
+            {
+                txtNombre.Text = "";
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void txtNombre_Leave(object sender, EventArgs e)
         {
-            Application.Exit();
+            if(txtNombre.Text == "")
+            {
+                txtNombre.Text = "NOMBRE";
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            MenuCrearUsuarios mcu = new MenuCrearUsuarios();
-            this.Hide();
-            mcu.Show();
+
+        }
+
+        private void txtApellido_Enter(object sender, EventArgs e)
+        {
+            if(txtApellido.Text == "APELLIDO")
+            {
+                txtApellido.Text = "";
+            }
+        }
+
+        private void txtApellido_Leave(object sender, EventArgs e)
+        {
+            if(txtApellido.Text == "")
+            {
+                txtApellido.Text = "APELLIDO";
+            }
         }
 
         private void txtEdad_Enter(object sender, EventArgs e)
@@ -53,77 +72,19 @@ namespace Tweak_it
             }
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            if (txtNombre.Text == "" || txtApellido.Text == "" || txtEdad.Text == "")
-            {
-                MessageBox.Show("Completa los campos");
-            }
-            else
-            {
-                connection.Open();
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                command.CommandText = "INSERT INTO info (Nombre, Apellido, Rol) VALUES ('" + txtNombre.Text + "', '" + txtApellido.Text + "','"+txtEdad.Text+"', '" + "Estudiante" + "')";
-                command.ExecuteNonQuery();
-                MessageBox.Show("El usuario del alumno ha sido creado correctamente");
-                connection.Close();
-
-                Menu_Profesor mp = new Menu_Profesor();
-                this.Hide();
-                mp.Show();
-            }
+            Menu_Profesor mp = new Menu_Profesor();
+            this.Hide();
+            mp.Show();
         }
 
-        private void txtNombre_Enter_1(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            if(txtNombre.Text == "NOMBRE")
-            {
-                txtNombre.Text = "";
-            }
+            Application.Exit();
         }
 
-        private void txtNombre_Leave_1(object sender, EventArgs e)
-        {
-            if(txtNombre.Text == "")
-            {
-                txtNombre.Text = "NOMBRE";
-            }
-        }
-
-        private void txtApellido_Enter_1(object sender, EventArgs e)
-        {
-            if(txtApellido.Text == "APELLIDO")
-            {
-                txtApellido.Text = "";
-            }
-        }
-
-        private void txtApellido_Leave_1(object sender, EventArgs e)
-        {
-            if(txtApellido.Text == "")
-            {
-                txtApellido.Text = "APELLIDO";
-            }
-        }
-
-        private void txtEdad_Enter_1(object sender, EventArgs e)
-        {
-            if(txtEdad.Text == "EDAD")
-            {
-                txtEdad.Text = "";
-            }
-        }
-
-        private void txtEdad_Leave_1(object sender, EventArgs e)
-        {
-            if(txtEdad.Text == "")
-            {
-                txtEdad.Text = "EDAD";
-            }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
+        private void btnCrear_Click(object sender, EventArgs e)
         {
             if(txtNombre.Text == "" || txtApellido.Text == "" || txtEdad.Text == "")
             {
@@ -136,13 +97,18 @@ namespace Tweak_it
                 command.Connection = connection;
                 command.CommandText = "INSERT INTO info (Nombre, Apellido, Edad, Rol) VALUES ('" + txtNombre.Text + "', '" + txtApellido.Text + "', '" + txtEdad.Text + "', '" + "Estudiante" + "')";
                 command.ExecuteNonQuery();
-                MessageBox.Show("El usuario del alumno ha sido creado correctamente");
+                MessageBox.Show("El usuario del alumno ha sido creado correctamente, ahora inicie sesión para empezar a jugar");
                 connection.Close();
 
-                MenuCrearUsuarios mcu = new MenuCrearUsuarios();
+                LOGIN lg = new LOGIN();
                 this.Hide();
-                mcu.Show();
+                lg.Show();
             }
+        }
+
+        private void CrearEstudiante_Load(object sender, EventArgs e)
+        {
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\user\Documents\GitHub\tweak-it\Tweak-it\BDD Tweak-It.accdb;Persist Security Info=False";
         }
     }
 }
--- Tweak-it/Tweak-it/Program.cs
+++ Tweak-it/Tweak-it/Program.cs
@@ -16,7 +16,7 @@ namespace Tweak_it
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CrearEstudiante());
            
         }
 
