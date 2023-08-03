843dba3756688cd22b3417731411da133ff890e7
BrunoAdaniya <110467314+BrunoAdaniya@users.noreply.github.com>
2023-08-03 13:35:54 -0300

    sjhdfgs

--- Tweak-it/Tweak-it/Program.cs
+++ Tweak-it/Tweak-it/Program.cs
@@ -17,7 +17,7 @@ namespace Tweak_it
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LOGIN());
+            Application.Run(new Form1());
            
         }
       
